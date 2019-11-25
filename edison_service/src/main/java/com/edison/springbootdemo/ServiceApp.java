40056c433ccb8187b88bc2c6fc0801b46b062a5a
edisonyoung1987 <edison_young@aliyun.com>
2019-11-25 22:47:44 +0800

    加入log4j2日志框架，还没完善【异步还没考虑进去】

--- edison_service/src/main/java/com/edison/springbootdemo/ServiceApp.java
+++ edison_service/src/main/java/com/edison/springbootdemo/ServiceApp.java
@@ -2,6 +2,8 @@ package com.edison.springbootdemo;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
@@ -12,7 +14,7 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication(exclude = {WebMvcAutoConfiguration.class})
 @EnableDubbo
 public class ServiceApp {
-
+    public static Logger logger= LoggerFactory.getLogger(ServiceApp.class);
     public static void main(String[] args) {
         System.out.println("ServiceApp starting...");
         SpringApplication.run(ServiceApp.class, args);
@@ -20,6 +22,6 @@ public class ServiceApp {
     }
     @PostConstruct
     public void init(){
-        System.out.println("执行InitOth.init()...");
+        logger.info("执行@postConstruct");
     }
 }
--- edison_service/src/main/java/com/edison/springbootdemo/config/InitOth.java
+++ edison_service/src/main/java/com/edison/springbootdemo/config/InitOth.java
@@ -1,12 +1,17 @@
 package com.edison.springbootdemo.config;
 
+import com.edison.springbootdemo.ServiceApp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 @Component
 public class InitOth {
+    public static Logger logger= LoggerFactory.getLogger(InitOth.class);
+
     @PostConstruct
     public void init(){
-        System.out.println("执行InitOth.init()0...");
+        logger.info("执行InitOth.@PostConstruct");
     }
 }
