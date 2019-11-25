5a7b67f6f744c5d7e680b8f0d2265839f90e7892
edisonyoung1987 <edison_young@aliyun.com>
2019-11-25 21:00:37 +0800

    提交pom.xml，将所有主pom.xml中的依赖都放到dependencymanagement中

--- edison_service/src/main/java/com/edison/springbootdemo/ServiceApp.java
+++ edison_service/src/main/java/com/edison/springbootdemo/ServiceApp.java
@@ -3,11 +3,12 @@ package com.edison.springbootdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
 import javax.annotation.PostConstruct;
 
 @MapperScan("com.edison.springbootdemo.mapper") //需要指定mapper地址，不然报错
-@SpringBootApplication()
+@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class})
 public class ServiceApp {
 
     public static void main(String[] args) {
