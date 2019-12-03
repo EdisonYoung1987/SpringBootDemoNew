26d7074ac007d2337e3ef5c440f1565386ad7916
edisonyoung1987 <edison_young@aliyun.com>
2019-12-03 16:24:56 +0800

    1. 增加一个通过redis实现的全局加锁工具类 2. 增加全局锁测试方法

---
+++ edison_common/src/main/java/com/edison/springbootdemo/utils/DistributeLocker.java
@@ -0,0 +1,79 @@
+package com.edison.springbootdemo.utils;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**基于redis实现的分布式锁。<p>
+ * CQRCB采用的是AOP注解方式对方法加锁，
+ * 个人感觉这样做虽然更方便，当是锁粒度过大-容易超时，除非将要加锁的片段拆分出去-会形成太多代码碎片</>*/
+@Component
+public class DistributeLocker {
+    @Resource(name = "normalRedisTemplate")
+    RedisTemplate<String,Object> redisTemplate;
+
+    //存在key则删除返回1，否则返回0，用于解锁
+    private static final RedisScript<Long> releaseScript=RedisScript.of("if redis.call('get',KEYS[1]) == ARGV[1] then return" +
+            " redis.call('del',KEYS[1]) else return 0 end",Long.class);
+
+    /**尝试加锁--不支持重入：<p>
+     * @param lockKey 加锁key
+     * @param lockSeconds 加锁时间：秒-考虑网络传输、业务处理耗时，最低单位秒
+     * 成功：返回uuid作为解锁依据<br>
+     * 失败：返回null*/
+    public String tryLock(String lockKey,long lockSeconds){
+        System.out.println(Thread.currentThread().getName()+"trylock...");
+        String uuid= UUID.randomUUID().toString();
+
+        //存在则设置key
+        Boolean res=redisTemplate.opsForValue().setIfAbsent(lockKey,uuid,lockSeconds, TimeUnit.SECONDS);
+        if(res!=null && res==true)
+            return uuid;
+        else
+            return null;
+    }
+
+    /**加锁--不支持重入：<p>
+     * @param lockKey 加锁key
+     * @param lockSeconds 加锁时间：秒-考虑网络传输、业务处理耗时，最低单位秒
+     * 成功：返回uuid作为解锁依据<br>
+     * 失败：返回null*/
+    public String getLock(String lockKey, long lockSeconds) {
+        System.out.println(Thread.currentThread().getName()+"getlock...");
+        String uuid;
+        while(null==(uuid=tryLock(lockKey,lockSeconds))) {
+            long expireTime=redisTemplate.getExpire(lockKey,TimeUnit.SECONDS);
+            System.out.println("当前lock ttl还有："+expireTime);
+            try {
+                Thread.sleep(expireTime*1000); //如果expireTime为0，则快速进入争抢
+            } catch (InterruptedException e) { }
+        }
+        if(Thread.currentThread().isInterrupted()) {//说明等待锁期间被interrupt过，之前中断信号被捕获，重新发起一次
+            Thread.currentThread().interrupt();
+        }
+        return uuid;
+    }
+    public String getLcokInterruptly(String lockKey, long lockSeconds,long waitSeconds) throws InterruptedException{
+        String uuid=null;
+        while(null==(uuid=tryLock(lockKey,lockSeconds))) {
+            long expireTime=redisTemplate.getExpire(lockKey,TimeUnit.SECONDS);
+             Thread.sleep(expireTime*1000);
+        }
+        return uuid;
+    }
+
+    /**解锁：用加锁返回的字串作为解锁依据
+     * @return  true - 成功 false - 失败*/
+    public boolean releaseLock(String lockKey,String uuid){
+        System.out.println(Thread.currentThread().getName()+"releaselock...");
+
+        long ret=redisTemplate.execute(releaseScript, Collections.singletonList(lockKey),uuid);
+        return ret==1;
+    }
+}
--- edison_service/src/test/java/com/edison/springbootdemo/ServiceAppTest.java
+++ edison_service/src/test/java/com/edison/springbootdemo/ServiceAppTest.java
@@ -1,6 +1,8 @@
 package com.edison.springbootdemo;
 
+import com.edison.springbootdemo.config.async.AsyncExecutor;
 import com.edison.springbootdemo.service.MicroSvcsImpl.EmployeeMicrosvcs;
+import com.edison.springbootdemo.utils.DistributeLocker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +15,41 @@ public class ServiceAppTest {
     @Autowired
     EmployeeMicrosvcs employeeMicrosvcs;
 
+    @Autowired
+    DistributeLocker distributeLocker;
+
+    @Autowired
+    AsyncExecutor asyncExecutor;
+
     @Test
     public void test(){
         System.out.println("test start");
         employeeMicrosvcs.findAll();
         System.out.println("test end");
 
+    }
+
+    @Test
+    public void testDistributeLock() throws Exception{
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                String name=Thread.currentThread().getName();
+                System.out.println(name+"开始加锁");
+                String uuid=distributeLocker.getLock("lockkey",10);
+                System.out.println(name+"获取锁，进入sleep...");
+                try {
+                    Thread.sleep(6000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("释放锁："+distributeLocker.releaseLock("lockkey",uuid));
+            }
+        };
+        //将任务提交给异步线程池执行
+        asyncExecutor.execute(runnable);
+        asyncExecutor.execute(runnable);
+
+        Thread.sleep(20000);
     }
 }
