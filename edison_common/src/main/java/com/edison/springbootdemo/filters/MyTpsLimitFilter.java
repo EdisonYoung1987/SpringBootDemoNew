59b2a98f680eff38b7430f0e6313c55d82e78aa1
edisonyoung1987 <edison_young@aliyun.com>
2020-03-10 10:35:08 +0800

    增加TpsLimitFilter进行tps控制3

--- edison_common/src/main/java/com/edison/springbootdemo/filters/MyTpsLimitFilter.java
+++ edison_common/src/main/java/com/edison/springbootdemo/filters/MyTpsLimitFilter.java
@@ -1,6 +1,19 @@
 package com.edison.springbootdemo.filters;
 
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Result;
+import org.apache.dubbo.rpc.RpcException;
 import org.apache.dubbo.rpc.filter.TpsLimitFilter;
 
 public class MyTpsLimitFilter extends TpsLimitFilter {
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+
+        if(invocation.getMethodName().matches("findAll")){//可以指定方法名进行控制
+            return super.invoke(invoker,invocation);
+        }
+
+        return invoker.invoke(invocation);
+    }
 }
